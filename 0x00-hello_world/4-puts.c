76e89f768227f0a313c9bacd15af0004a7274a62
fhonsiah <fhonsiah@gmail.com>
2022-07-04 16:13:50 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+/*
+ *Program: 4-puts.c
+ *The program prints a text to the standard output
+ *Author:Faustina Honsiah <fhonsiah@gmail.com>
+ */
+#include <stdio.h>
+/**
+ *main - Entry point of the program
+ *prints -\"Programming is like building a multilingual puzzle
+ *Return: 0 if the program runs without errors.
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
