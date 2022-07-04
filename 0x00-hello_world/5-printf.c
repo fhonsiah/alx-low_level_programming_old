39065b57d54c42d685fc770a06443fff43f50f23
fhonsiah <fhonsiah@gmail.com>
2022-07-04 16:29:55 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+/*
+ *Program:5-printf.c
+ *The program prints a text to the standard output
+ *Author:Faustina Honsiah <fhonsiah@gmail.com>
+ */
+#include <stdio.h>
+/**
+ *main - Entry point of the program
+ *Return: 0 if the program runs without errors
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcone is a place of art,\n");
+return (0);
+}
