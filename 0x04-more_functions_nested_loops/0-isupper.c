10cef8ba11bbc9b4e8feb4e4f5b84df1dd90b927
fhonsiah <fhonsiah@gmail.com>
2022-07-08 13:38:54 -0700

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+/*
+ *Program: 0.isupper
+ *The Program checks for uppercase character
+ *with a function.
+ *Author:Faustina Honsiah <fhonsiah@gmail.com>
+ */
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+/**
+ *main - check the code
+ *
+ *Return: Always 0.
+ */
+int main(void)
+{
+char c;
+c = 'A';
+printf("%c: %d\n", c, _isupper(c));
+c = 'a';
+printf("%c: %d\n", c, _isupper(c));
+return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,5 @@
+
+#  0x02-functions_nested_loops
+
+## 0. _putchar
+Program that prints "putchar", followed by a new line.
