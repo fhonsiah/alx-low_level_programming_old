ebf03e0bac9d4456e2869c4818b13339c71d9166
fhonsiah <fhonsiah@gmail.com>
2022-07-09 15:51:11 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+/*
+ *Program: 2-print_alphabet_x10.c
+ *The program prints 10 times the alphabet,
+ *in lowercase,followed by a new line
+ *Author:Faustina Honsiah
+ */
+#include "main.h"
+/**
+ *print_alphabet_x10 - print the alphabet 10 times
+ */
+void print_alphabet_x10(void)
+{
+char alphabet = 'a';
+int counter = 1;
+while (counter <= 10)
+{
+while (alphabet <= 'z')
+{
+_putchar(alphabet);
+alphabet++;
+}
+counter++;
+alphabet = 'a';
+_putchar('\n');
+}
+}
